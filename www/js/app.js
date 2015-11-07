388b34680c966169906491af4ad90ea4204fa888
Vassilis <vapits@gmail.com>
2015-11-07 22:24:19 +0100

    add home route

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('wo', ['ionic', 'wo.controllers'])
 
 .run(function($ionicPlatform) {
@@ -25,11 +19,10 @@ angular.module('wo', ['ionic', 'wo.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
   });
 
   // if none of the above states are matched, use this as the fallback
