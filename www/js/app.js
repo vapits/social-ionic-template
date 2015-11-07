b95279216e87e7eb62929e8633b5b14aef334a06
Vassilis <vapits@gmail.com>
2015-11-08 00:05:37 +0100

    code styling

--- www/js/app.js
+++ www/js/app.js
@@ -1,40 +1,41 @@
 angular.module('wo', ['ionic', 'wo.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-    .state('home', {
-      url: '/home',
-      templateUrl: 'templates/menu.html',
-      controller: 'AppCtrl'
-  })
+            .state('home', {
+                url: '/home',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
 
-      .state('home.feed', {
-        url: '/feed',
-        views: {
-          'menuContent' :{
-            templateUrl: 'templates/home.html',
-            controller: 'HomeCtrl'
-          }
-        }
-      });
+            .state('home.feed', {
+                url: '/feed',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeCtrl'
+                    }
+                }
+            });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
-});
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/home/feed');
+    });
